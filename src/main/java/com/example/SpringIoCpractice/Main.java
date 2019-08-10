8f3e082050ac89741b4ab011473d46afe68d803b
sdp403213485 <45375846+sdp403213485@users.noreply.github.com>
2019-08-10 10:45:23 +0800

    Update Main.java

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,17 @@
+package com.example.SpringIoCpractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
 //TODO
+@Component
+public class Main{
+
+    public void testMain(){
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("hello.xml");
+       HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("HelloWorldBean");
+        helloWorld.getHello();
+    }
+}
